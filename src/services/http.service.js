3db8a0cd52e1103102da978df7bf0848131c74a3
vladimirbalin <vladimirzq@Gmail.com>
2023-01-11 21:26:44 +0700

    Add response interceptor which redirects when 401 or 419 status

--- src/services/http.service.js
+++ src/services/http.service.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import authService from "@/services/auth.service";
 
 let config = {
     baseURL: process.env.VUE_APP_API_DOMAIN + '/api/',
@@ -6,5 +7,19 @@ let config = {
 }
 
 const httpService = axios.create(config);
+
+httpService.interceptors.response.use(
+    response => {
+        return response;
+    },
+    async error => {
+        if (error.response.status === 401 ||
+            error.response.status === 419)
+        {
+            await authService.logout()
+        }
+        return Promise.reject(error);
+    }
+);
 
 export default httpService;
